9496d71e0f06388d1eb4c63662a21b42a0d38191
Isuru Wickramasinghe <iwickram@ualberta.ca>
2022-05-05 11:49:07 -0600

    Deleted AboutView.vue Added ExploreView, JoinView, ProfileView updated router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,13 +8,23 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/explore',
+      name: 'explore',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" */ '../views/ExploreView.vue')
+    },
+    {
+        path: '/join',
+        name: 'join',
+        component: () => import('../views/JoinView.vue')
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('../views/ProfileView.vue')
+    }
 ]
 
 const router = createRouter({
