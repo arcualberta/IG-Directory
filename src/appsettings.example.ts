b52275ad40d8090cdf26233adfa5aee27c4c527a
Isuru Wickramasinghe <iwickram@ualberta.ca>
2022-05-12 09:56:46 -0600

    appsettings.example.ts updated

--- src/appsettings.example.ts
+++ src/appsettings.example.ts
@@ -14,6 +14,21 @@ export default {
     }
 }
 export enum SearchResultFieldMapping {
-    TITLE = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_04e54156-f480-41e6-a76a-210456b66499_ts",
-    POSITION = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_016caaba-a0c7-4acc-b0c2-813376b2f32c_ts"
+    PRONOUNS = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_69d866ae-94bc-4701-ab72-998a4d8bc070_ts",
+    NAME = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_04e54156-f480-41e6-a76a-210456b66499_ts",
+    EMAIL = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_4fa650bc-a20b-4a39-b7d6-40728f2461dd_ts",
+    POSITION = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_016caaba-a0c7-4acc-b0c2-813376b2f32c_ts",
+    ORGANIZATION = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_98b5b06d-b4e8-4a48-ad2c-7a36b9857064_ts",
+    PERSON_OF_COLOR = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_d13046af-fe79-49f8-9eda-77fc5525a611_ts",
+    DISABILITY = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_b7f7b8f0-f2c9-422e-a91d-217fa4c5da8d_ts",
+    ETHNICITY = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_b2b63066-a17e-45dd-a3ae-a6c1f815de7b_ts",
+    GENDER_IDENTITY = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_deffa707-cd34-4f2b-9fcd-2e8c5161f5c9_ts",
+    COMMUNITY_BASED_PROJECTS = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_f3964ff7-9c8b-4286-a8ab-8020d9fc421a_ts",
+    DISPLAY_ON_PROFILE = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_172f88ff-7c1e-4a81-aaca-0a5483af8d51_ts",
+    KEYWORDS = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_fb7a7395-969b-458e-898b-7cb07efabf33_ts",
+    ADDITIONAL_KEYWORDS = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_d860b5ef-93c2-4c88-a213-7746873fe104_ts",
+    DESCRIPTION = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_0774a8bf-bcf0-4733-8480-85a3b9e1451e_ts",
+    EXTERNAL_LINKS = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_44f39fe7-f3af-47da-a2b6-f610c2f0eda3_ts",
+    CONSENT = "data_49a7a1d3-0194-4703-b3d8-747acbf3bbfa_84f22416-34ac-4451-bc59-e403bcd684fa_ts"
+
 }
