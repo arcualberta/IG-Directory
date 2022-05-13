2ed917323e081178f4202aa59993a77dbd9cfe29
mruaini <melaniar@hotmail.com>
2022-05-13 12:52:55 -0600

    TASK 1165: Add KeywordList component

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { defineComponent, computed, onMounted } from 'vue';
     import { useStore } from 'vuex';
-
+    import KeywordList from "../components/KeywordList.vue"
     import { search, FreeTextSearch } from '@arcualberta/catfish-ui';
+    import config from '../appsettings';
 
     export default defineComponent({
         name: 'ProfileView',
@@ -11,23 +12,24 @@
         },
         components: {
             FreeTextSearch,
+            KeywordList
         },
         setup() {
             const store = useStore();
-
+             store.dispatch(search.Actions.INIT_FILTER);
             onMounted(() => store.dispatch(search.Actions.FRESH_SEARCH))
 
             return {
                 state: computed(() => store.state),
                 keywordQueryModel: computed(() => store.state.search.keywordQueryModel),
                 searchResults: computed(() => store.state.search.searchResult),
-
+                 colorList:computed(()=>config.hexColorList)
             }
         }
     });
 </script>
 <template>
-    <FreeTextSearch />
+    
     <div class="Profile">
         <div class="background-grey-researcher">
             <div class="results">
@@ -46,7 +48,8 @@
             </div>
         </div>
         <div class="right-content-researcher">
-
+            <FreeTextSearch />
+             <KeywordList :model="keywordQueryModel" :hexColorList="colorList" :className="'keywordContainerSmall'" />
         </div>
         <div class="explore-related">
 
