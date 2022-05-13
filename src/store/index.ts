1c83fde9f1e1124f81b8afb3dd1427cf3f332d4a
Isuru Wickramasinghe <iwickram@ualberta.ca>
2022-05-13 08:25:09 -0600

    Getters completed

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,8 +11,22 @@ export default createStore({
     state: {
     },
     getters: {
-        getTitle: () => (item: any) => item?.solrFields[SearchResultFieldMapping.TITLE],
+        getPronouns: () => (item: any) => item?.solrFields[SearchResultFieldMapping.PRONOUNS],
+        getName: () => (item: any) => item?.solrFields[SearchResultFieldMapping.NAME],
+        getEmail: () => (item: any) => item?.solrFields[SearchResultFieldMapping.EMAIL],
         getPosition: () => (item: any) => item?.solrFields[SearchResultFieldMapping.POSITION],
+        getOrganization: () => (item: any) => item?.solrFields[SearchResultFieldMapping.ORGANIZATION],
+        getPersonOfColor: () => (item: any) => item?.solrFields[SearchResultFieldMapping.PERSON_OF_COLOR],
+        getDisability: () => (item: any) => item?.solrFields[SearchResultFieldMapping.DISABILITY],
+        getEthnicity: () => (item: any) => item?.solrFields[SearchResultFieldMapping.ETHNICITY],
+        getGenderIdentity: () => (item: any) => item?.solrFields[SearchResultFieldMapping.GENDER_IDENTITY],
+        getCommunityBasedProjects: () => (item: any) => item?.solrFields[SearchResultFieldMapping.COMMUNITY_BASED_PROJECTS],
+        getDisplayOnProfile: () => (item: any) => item?.solrFields[SearchResultFieldMapping.DISPLAY_ON_PROFILE],
+        getKeywordReference: () => (item: any) => item?.solrFields[SearchResultFieldMapping.KEYWORDS_REFERENCE],
+        getAdditionalKeywords: () => (item: any) => item?.solrFields[SearchResultFieldMapping.ADDITIONAL_KEYWORDS],
+        getDescription: () => (item: any) => item?.solrFields[SearchResultFieldMapping.DESCRIPTION],
+        getExternalLinks: () => (item: any) => item?.solrFields[SearchResultFieldMapping.EXTERNAL_LINKS],
+        getConsent: () => (item: any) => item?.solrFields[SearchResultFieldMapping.CONSENT],
     },
     mutations: {
     },
