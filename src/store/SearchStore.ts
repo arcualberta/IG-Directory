541e39adba932dd0ae3f998594e2c34fc245062e
KamalRanaweera <ranaweer@ualberta.ca>
2022-05-19 23:36:55 -0600

    Implemented HomeView.vue using Pinia.

--- src/store/SearchStore.ts
+++ src/store/SearchStore.ts
@@ -1,13 +1,21 @@
 import { defineStore } from 'pinia';
+import { Guid } from 'guid-typescript';
 
 import { search } from '@arcualberta/catfish-ui';
 
 export const useSearchStore = defineStore('SearchStore', {
     state: () => ({
+        templateId: null as null | Guid,
+        collectionId: null as null | Guid,
+        groupId: null as null | Guid,
+        stateIdRestrictions: [] as Guid[],
+        queryParams: null as null | string,
+        offset: 0,
+        pageSize: 25,
         queryModelRetrieverApiUrl: null as null | string,
         queryApiUrl: null as null | string,
         keywordQueryModel: null as null | search.KeywordQueryModel,
-        freeTextSearchEntries: [] as string[],
+        searchText: null as null | string,
         searchResult: {
             first: 0,
             last: 0,
@@ -26,6 +34,41 @@ export const useSearchStore = defineStore('SearchStore', {
                     .then(data => {
                         this.keywordQueryModel = data;
                     });
+        },
+        fetchData() {
+            const api = this.queryApiUrl + 'keywordsearch';
+            console.log("Item Load API: ", api)
+
+            const formData = new FormData();
+
+            if (this.templateId)
+                formData.append("templateId", this.templateId.toString());
+
+            if (this.collectionId)
+                formData.append("collectionId", this.collectionId.toString());
+
+            if (this.groupId)
+                formData.append("groupId", this.groupId.toString())
+
+            if (this.searchText)
+                formData.append("searchText", this.searchText as string);
+
+            formData.append("offset", this.offset.toString());
+            formData.append("max", this.pageSize.toString());
+            formData.append("queryParams", JSON.stringify(this.keywordQueryModel));
+
+
+            fetch(api, {
+                method: 'POST', // or 'PUT'
+                body: formData
+            })
+                .then(response => response.json())
+                .then(data => {
+                    this.searchResult = data;
+                })
+                .catch((error) => {
+                    console.error('Item Load API Error:', error);
+                });
         }
     }
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,23 +9,11 @@
         </div>
         <KeywordList :model="keywordQueryModel" :hexColorList="colorList" />
 
-        <h3>Keyword Query Model</h3>
-        {{JSON.stringify(keywordQueryModel)}} <br />
-        Result count: {{searchStore.resultCount}} of {{searchStore.searchResult.count}}
-
-
-        <h3>Search Store</h3>
-        {{JSON.stringify(searchStore)}}
-
-        <h3>Profile Store</h3>
-        {{JSON.stringify(profileStore)}}
-
     </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent, computed, onMounted } from 'vue';
-    import { useStore } from 'vuex';
+    import { defineComponent, computed } from 'vue';
 
     import { useSearchStore, useProfileStore } from '../store'
 
@@ -46,16 +34,11 @@
         setup() {
             const searchStore = useSearchStore();
             const profileStore = useProfileStore();
-
-            const store = useStore();
-
-            store.dispatch(search.Actions.INIT_FILTER)
            
             return {
                 searchStore,
                 profileStore,
-                state: computed(() => store.state),
-                keywordQueryModel: computed(() => store.state.search.keywordQueryModel),
+                keywordQueryModel: computed(() => searchStore.keywordQueryModel),
                 colorList: computed(()=>config.hexColorList)
             }
         }
