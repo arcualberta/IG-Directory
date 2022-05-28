e0cf08fbcb6f5e38ef2ea041fdd8ef8d10c5a31f
Isuru Wickramasinghe <iwickram@ualberta.ca>
2022-05-27 19:00:54 -0600

    PopupContainer.vue component created

---
+++ src/components/PopupContainer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="popup">
+        <div class="ui-dialog">
+            <button class="popup-close" @click="TogglePopup()">Close</button>
+            <slot/>
+        </div>
+    </div>   
+</template>
+
+<script lang="ts">
+    export default {
+        props: ['TogglePopup']
+
+    }
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,17 @@
 <template>
+    <button class="info-rectangle" @click="TogglePopup()">Info</button>
+    <PopupContainer v-if="popupTrigger" :TogglePopup ="() => TogglePopup()">
+        <div>
+            <p>
+                Intersections of Gender is committed to using intersectional experience and excellence to build and sustain for the public good and to bring together wide-ranging initiatives to advance knowledge and inspire engaged citizenship around the world.
+                Learn more about IG
+            </p>
+            <p>
+                This database features faculty, instructors and students at University of Alberta doing work at the intersections of gender. The database contains profiles for more than 250 researchers.
+            </p>
+        </div>
+        
+    </PopupContainer>
     <div class="home-body">
         <h1 class="title">Explore our<br />Researcher Directory.</h1>
         <p class="explore-text">
@@ -9,12 +22,13 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent, computed } from 'vue';
+    import { defineComponent, computed, ref } from 'vue';
 
     import { useSearchStore } from '../store'
 
     import { search } from '@arcualberta/catfish-ui';
-    import KeywordList from "../components/KeywordList.vue"
+    import KeywordList from "../components/KeywordList.vue" 
+    import PopupContainer from "../components/PopupContainer.vue"
     import config from '../appsettings';
 
     export default defineComponent({
@@ -23,13 +37,18 @@
             search
         },
         components: {
-            KeywordList
+            KeywordList,
+            PopupContainer,
         },
         setup() {
             const searchStore = useSearchStore();
-           
+            const popupTrigger = ref(false);
+            //const TogglePopup = (trigger) => { popupTrigger.value[trigger] = !popupTrigger.value[trigger] };
             return {
                 searchStore,
+                PopupContainer,
+                popupTrigger,
+                TogglePopup: () => ( popupTrigger.value = !popupTrigger.value ),
                 //keywordQueryModel: computed(() => searchStore.keywordQueryModel),
                 colorList: computed(()=>config.hexColorList)
             }
