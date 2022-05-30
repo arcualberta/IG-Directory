4f9b232cf2f5c43242e20755ceb25b3f623c2775
mruaini <melaniar@hotmail.com>
2022-05-30 12:36:12 -0600

    Modified TextField, add MultivalueText and SingleText editor template

---
+++ src/components/form/components/editor/MultivalueText.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { defineComponent, PropType } from 'vue'
+   
+    import * as models from '../../models'
+	
+    import SingleText from './SingleText.vue'
+
+    export default defineComponent({
+        name: "MultivalueText",
+        props: {
+            model: {
+                type: null as PropType<models.TextCollection> | null,
+                required: true
+			}
+			//validationStatus: {
+			//	type: null as PropType<eValidationStatus> | null,
+			//	required: true
+           // }
+        },
+        components: {
+            SingleText
+        }
+    });
+</script>
+
+<template>
+    <div v-for="val in model?.values?.$values" :key="val.id">
+       
+        <SingleText :model="val"   />
+    </div>
+
+</template>
+
---
+++ src/components/form/components/editor/SingleText.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { defineComponent, PropType } from 'vue'
+	
+	
+	import * as models from '../../models'
+
+    export default defineComponent({
+        name: "SingleText",
+        props: {
+            model: {
+                type: null as PropType<models.Text> | null,
+                required: true
+			},
+			
+        },
+	
+    });
+</script>
+
+<template>
+	<input type="text"  />	
+</template>
+
