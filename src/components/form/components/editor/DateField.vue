794e0e05fb195ac09684ecd93a67d5f496abac41
mruaini <melaniar@hotmail.com>
2022-05-30 14:55:22 -0600

    Modified DecimalField and dateField

--- src/components/form/components/editor/DateField.vue
+++ src/components/form/components/editor/DateField.vue
@@ -1,9 +1,12 @@
 <script lang="ts">
     import { defineComponent, PropType } from "vue";
     import * as models from '../../models'
-
+     import SingleText from './SingleText.vue'
     export default defineComponent({
         name: "DateField",
+        components:{
+           SingleText
+        },
         props: {
             model: {
                 type: null as PropType<models.MonolingualTextField> | null,
@@ -21,8 +24,8 @@
 
 
 <template>
-    DateField
-    <!--<div v-for="val in model?.values?.$values">
-        <Text :model="val" :is-multiline="false" :is-rich-text="false" :validation-status="validationStatus" field="date" />
-    </div>-->
+    
+    <div v-for="val in model?.values?.$values" :key="val.id">
+        <SingleText :model="val" :isMultiline="false"  fieldType="date" />
+    </div>
 </template>
