25bf5593bc2de01bd5aa07461ab6bdd154c920a3
KamalRanaweera <ranaweer@ualberta.ca>
2022-06-08 23:34:36 -0600

    Restored form validation. Sends files to be uploaded as multi-part form data.

--- src/store/FormStore.ts
+++ src/store/FormStore.ts
@@ -1,10 +1,10 @@
 import { defineStore } from 'pinia';
 //import { Guid } from 'guid-typescript';
 
-import { search } from '@arcualberta/catfish-ui';
-import { form } from '@arcualberta/catfish-ui';
+import { search, form } from '@arcualberta/catfish-ui';
 
 import config from '../appsettings';
+import { FieldContainer } from '@arcualberta/catfish-ui/dist/types/src/lib-components/form/models';
 
 export const useFormStore = defineStore('FormStore', {
     state: () => ({
@@ -36,32 +36,19 @@ export const useFormStore = defineStore('FormStore', {
         },
         submitForm() {
 
-            //const api = 'https://catfish-test.artsrn.ualberta.ca/applets/api/itemtemplates/bd35d406-3399-40af-bc72-c7b5813ee9b1/data-form/49a7a1d3-0194-4703-b3d8-747acbf3bbfa'
             const api = `${config.dataServiceApiRoot}items/?itemTemplateId=${config.dataAttributes.templateId}&groupId=${config.dataAttributes.groupId}&collectionId=${config.dataAttributes.collectionId}` //'To the submit-form action on the Items controller PostAsync method';
-            ///applets/api/items/?itemTemplateId=${store.state.itemTemplateId}&groupId=${store.state.groupId ? store.state.groupId : ""}&collectionId=${store.state.collectionId}`;
-
-            //Update the following fetch call similar to the Actions.SUBMIT_FORM action on the Catfish.Applets, form-submission actions
-            //so that it will pass on the "this.form" in this FormStore and get the response.
-            //If the response is successful, redirect the user (router.push) to the JoinConfirmationView view. 
-            //If the post cal failed, display "Sibmission failed" near the "Submit" button on the Join Us page.
 
             const formData = new FormData();
             //Setting the serialized JSON form model to the datamodel variable in formData
             formData.append('datamodel', JSON.stringify(this.form));
             console.log('datamodel - ', JSON.stringify(this.form))
 
-            ////Adding all attachments uploaded to the files variable in formData
-            //for (const key in store.state.flattenedFileModels) {
-            //    if (store.state.flattenedFileModels[key].length > 0) {
-            //        store.state.flattenedFileModels[key].forEach(file => {
-            //            console.log("File: ", file.name)
-            //            formData.append('files', file);
-            //            formData.append('fileKeys', key);
-            //        })
-            //    }
-            //}
-
-
+            //Adding all attachments uploaded to the files variable in formData
+            const fileReferences = form.helpers.getFileReferences(this.form as FieldContainer);
+            fileReferences.forEach(fileRef => {
+                formData.append('files', fileRef.file);
+                formData.append('fileKeys', fileRef.fieldId.toString() as unknown as string);
+           })
 
             fetch(api,
                 {
